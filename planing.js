d869690f3b877a2a63ccfe8379a1d15911057dd8
ranayash24 <homehelp24022000@gmail.com>
2021-07-14 18:20:58 +0530

    something do

---
+++ planing.js
@@ -0,0 +1,25 @@
+//Book my show planning 
+// ->we need a reusable carousel 
+//->hero carosel
+// -> comman carosel
+//-> cast and crew small carousel
+
+
+
+// Pages
+ //-> home page
+ //->movie page
+ //-> categories page
+ //->auth page
+ //-> razorpay payment gateway
+ 
+ 
+ //plan -> how are we builidng it??😕
+ //layouts 
+ //routes
+ //pages 
+ // components
+
+ //HOc
+ // Higer order components
+ 
